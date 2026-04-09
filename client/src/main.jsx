d2362b37d5ce6d029efca95fb6ca918edbdd59e4
garry3395 <g9876430@gmail.com>
2026-04-09 18:19:41 +0200

    nw

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,28 +8,45 @@ const cubeMaterial=new THREE.MeshBasicMaterial({color:"red"})
 
 const cubeMesh=new THREE.Mesh(cubeGemotry,
   cubeMaterial)
+                                                                           
 console.log(cubeMesh)
   console.log(scene)   
   scene.add(cubeMesh)
 
-  const camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 3, 200);
+  const camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 3, 200); 
+ 
+/*   const asceptRatio= window.innerWidth / window.innerHeight
+
+  const camera=new THREE.OrthographicCamera(-1*asceptRatio,1*asceptRatio,1,-1,0.1,200) */
   camera.position.z=5;
+
    scene.add(camera)
 const canvas=document.querySelector('canvas.threejs')
-
-const renderer= new THREE.WebGLRenderer({canvas:canvas})
+console.log(window.devicePixelRatio)
+const renderer= new THREE.WebGLRenderer({canvas:canvas,
+  antialias:true
+})
 const controls=new OrbitControls(camera,canvas)
 controls.enableDamping =true
 controls.autoRotate= true
+window.addEventListener('resize',()=>{
+  camera.aspect=window.innerWidth/window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth,window.innerHeight)
+  const maxPixelRatio=Math.min(window.devicePixelRatio,2)
+  renderer.setPixelRatio(maxPixelRatio)
+
+})
 const renderloop=()=>{
+ 
   renderer.render(scene,camera)
 
 window.requestAnimationFrame(renderloop)
+
   controls.update()
 }
 
 renderloop()
-renderer.setSize(window.innerWidth,window.innerHeight)
 
 console.log(canvas)
 
